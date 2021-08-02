2f81a131d4311bbbe4ae5344fd3765b8b697a351
Huy <s3825059@rmit.edu.vn>
2021-08-02 08:49:14 +0700

    Finish simple styling for the timer

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <span>
+          00 Days
+        </span>
+
+        <span>
+          00 hours
+        </span>
+
+        <span>
+          00 minutes
+        </span>
+
+        <span>
+          00 seconds
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
