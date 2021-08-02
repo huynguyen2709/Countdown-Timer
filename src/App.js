b8ddf6da414775ca358b6422a67fd4fe375cf67d
Huy <s3825059@rmit.edu.vn>
2021-08-02 09:10:48 +0700

    Finish the function using useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [timer, setTimer] = useState({
+    day: '00',
+    hour: '00',
+    minute: '00',
+    second: '00',
+  });
+
+  const now = new Date().getTime();
+  const chooseTime = new Date('2021-08-03').getTime();
+  const gap = chooseTime - now;
+
+  const day = Math.floor((gap/ 1000 / 60 / 60 / 24) % 24);
+  const hour = Math.floor((gap / 1000 / 60 / 60) % 60);
+  const minute = Math.floor((gap / 1000 / 60) % 60);
+  const second = Math.floor((gap/ 1000) % 60);
+
+
+  useEffect(() => {
+    setInterval( activeSetTimer, 1000);
+  }, [timer])
+
+  const activeSetTimer = () => {
+    setTimer({
+      day: day,
+      hour: hour,
+      minute: minute,
+      second: second,
+    });
+  }
+
   return (
     <div className="App">
       <div className='container'>
         <span>
-          00 Days
+          {day} Days
         </span>
 
         <span>
-          00 hours
+          {hour} hours
         </span>
 
         <span>
-          00 minutes
+          {minute} minutes
         </span>
 
         <span>
-          00 seconds
+          {second} seconds
         </span>
       </div>
     </div>
